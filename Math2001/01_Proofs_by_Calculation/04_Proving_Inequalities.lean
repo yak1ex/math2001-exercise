7f51c285f38c8c1bd0e61a0e991dbd1f9edd19fc
Yak! <yak1ex@users.noreply.github.com>
2025-02-22 15:13:40 +0900

    Add soultions for example 1.4.

--- Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
+++ Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
@@ -20,14 +20,18 @@ example {x y : ℤ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 :=
 -- Exercise: replace the words "sorry" with the correct Lean justification.
 example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
   calc
-    r = (s + r + r - s) / 2 := by sorry
-    _ ≤ (3 + (s + 3) - s) / 2 := by sorry
-    _ = 3 := by sorry
+    r = (s + r + r - s) / 2 := by ring
+    _ ≤ (3 + (s + 3) - s) / 2 := by rel[h2,h1]
+    _ = 3 := by ring
 
 -- Example 1.4.3
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
 example {x y : ℝ} (h1 : y ≤ x + 5) (h2 : x ≤ -2) : x + y < 2 :=
-  sorry
+  calc
+    x + y ≤ x + (x + 5) := by rel[h1]
+    _ = x + x + 5 := by ring
+    _ ≤ (-2) + (- 2) + 5 := by rel[h2]
+    _ < 2 := by numbers
 
 -- Example 1.4.4
 -- Exercise: replace the words "sorry" with the correct Lean justification.
@@ -36,28 +40,35 @@ example {u v x y A B : ℝ} (h1 : 0 < A) (h2 : A ≤ 1) (h3 : 1 ≤ B) (h4 : x �
     u * y + v * x + u * v < 3 * A * B :=
   calc
     u * y + v * x + u * v
-      ≤ u * B + v * B + u * v := by sorry
-    _ ≤ A * B + A * B + A * v := by sorry
-    _ ≤ A * B + A * B + 1 * v := by sorry
-    _ ≤ A * B + A * B + B * v := by sorry
-    _ < A * B + A * B + B * A := by sorry
-    _ = 3 * A * B := by sorry
+      ≤ u * B + v * B + u * v := by rel[h5,h4]
+    _ ≤ A * B + A * B + A * v := by rel[h8,h9]
+    _ ≤ A * B + A * B + 1 * v := by rel[h2]
+    _ ≤ A * B + A * B + B * v := by rel[h3]
+    _ < A * B + A * B + B * A := by rel[h9]
+    _ = 3 * A * B := by ring
 
 -- Example 1.4.5
 -- Exercise: replace the words "sorry" with the correct Lean justification.
 example {t : ℚ} (ht : t ≥ 10) : t ^ 2 - 3 * t - 17 ≥ 5 :=
   calc
     t ^ 2 - 3 * t - 17
-      = t * t - 3 * t - 17 := by sorry
-    _ ≥ 10 * t - 3 * t - 17 := by sorry
-    _ = 7 * t - 17 := by sorry
-    _ ≥ 7 * 10 - 17 := by sorry
-    _ ≥ 5 := by sorry
+      = t * t - 3 * t - 17 := by ring
+    _ ≥ 10 * t - 3 * t - 17 := by rel[ht]
+    _ = 7 * t - 17 := by ring
+    _ ≥ 7 * 10 - 17 := by rel[ht]
+    _ ≥ 5 := by numbers
 
 -- Example 1.4.6
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
 example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
-  sorry
+  calc
+    n ^ 2 = n * n := by ring
+    _ ≥ 5 * n := by rel[hn]
+    _ = 2 * n + 3 * n := by ring
+    _ ≥ 2 * n + 3 * 5 := by rel[hn]
+    _ = 2 * n + 15 := by ring
+    _ = 2 * n + 11 + 4 := by ring
+    _ > 2 * n + 11 := by extra
 
 -- Example 1.4.7
 example {m n : ℤ} (h : m ^ 2 + n ≤ 2) : n ≤ 2 :=
@@ -70,10 +81,10 @@ example {m n : ℤ} (h : m ^ 2 + n ≤ 2) : n ≤ 2 :=
 -- Exercise: replace the words "sorry" with the correct Lean justification.
 example {x y : ℝ} (h : x ^ 2 + y ^ 2 ≤ 1) : (x + y) ^ 2 < 3 :=
   calc
-    (x + y) ^ 2 ≤ (x + y) ^ 2 + (x - y) ^ 2 := by sorry
-    _ = 2 * (x ^ 2 + y ^ 2) := by sorry
-    _ ≤ 2 * 1 := by sorry
-    _ < 3 := by sorry
+    (x + y) ^ 2 ≤ (x + y) ^ 2 + (x - y) ^ 2 := by extra
+    _ = 2 * (x ^ 2 + y ^ 2) := by ring
+    _ ≤ 2 * 1 := by rel[h]
+    _ < 3 := by numbers
 
 -- Example 1.4.9
 -- Exercise: replace the words "sorry" with the correct Lean justification.
@@ -81,12 +92,12 @@ example {a b : ℚ} (h1 : a ≥ 0) (h2 : b ≥ 0) (h3 : a + b ≤ 8) :
     3 * a * b + a ≤ 7 * b + 72 :=
   calc
     3 * a * b + a
-      ≤ 2 * b ^ 2 + a ^ 2 + (3 * a * b + a) := by sorry
-    _ = 2 * ((a + b) * b) + (a + b) * a + a := by sorry
-    _ ≤ 2 * (8 * b) + 8 * a + a := by sorry
-    _ = 7 * b + 9 * (a + b) := by sorry
-    _ ≤ 7 * b + 9 * 8 := by sorry
-    _ = 7 * b + 72 := by sorry
+      ≤ 2 * b ^ 2 + a ^ 2 + (3 * a * b + a) := by extra
+    _ = 2 * ((a + b) * b) + (a + b) * a + a := by ring
+    _ ≤ 2 * (8 * b) + 8 * a + a := by rel[h3]
+    _ = 7 * b + 9 * (a + b) := by ring
+    _ ≤ 7 * b + 9 * 8 := by rel[h3]
+    _ = 7 * b + 72 := by ring
 
 -- Example 1.4.10
 example {a b c : ℝ} :
